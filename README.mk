e67177aeaf70f3f0feedba7d05227e2ef3b49e32
Gabriel Salviato <gasalviato@gmail.com>
2026-03-16 11:17:08 -0300

    readme

--- README.mk
+++ README.mk
@@ -1,8 +1,8 @@
 Este repositório é onde coloco em execução boas práticas de código. O objetivo é treinar a escrita de códigos mais limpos, coesos e menos acoplados.
 
-Cada arquivo/módulo deste projeto foca em refatorar um problema comum aplicando uma ou mais regras específicas de boas práticas. Abaixo está a descrição do que foi explorado em cada um deles.
+Cada arquivo deste projeto foca em refatorar um problema comum aplicando uma ou mais regras específicas de boas práticas. Abaixo está a descrição do que foi explorado em cada um deles.
 
-Não usando FLOAT (FLOAT.py)
+Contra FLOAT (FLOAT.py)
 Tipos primitivos de ponto flutuante (float, double) sofrem de problemas de precisão, o que pode causar bugs graves, especialmente lidando com dinheiro.
 Pratiquei a substituição de float para armazenar valores monetários em inteiros/centavos.
 
@@ -15,11 +15,12 @@ Regra 1 Apenas um nível de indentação por método de extração de métodos p
 Regra 2. Uso de Early Return para eliminar fluxos condicionais complexos e aninhados.
 
 Object Calisthenics - Lei 3 (ObjectCalRule3.py)
-Regra 3 Envolva todos os primitivos e Strings.	 Criação de Value Objects (Objetos de Valor). Em vez de passar um simples string ou int que não tem significado de domínio, criei classes específicas como Email, CPF e Idade para garantir a validação e encapsular o comportamento logo na origem.
+	 
+Em vez de passar um simples string ou int que não tem significado de domínio, criei classes específicas como Nome e Idade para garantir a validação e encapsular o comportamento logo na origem.
 
 Object Calisthenics - Leis 8 e 9 (ObjectCalRule8&9.py)
-Regra 8. Promoção de alta coesão desmontando classes Deus (God classes) em objetos menores e mais focados.
-Regra 9. O foco passou a ser em pedir para o objeto fazer algo com seus próprios dados, em vez de arrancar os dados dele para manipulá-los em outro lugar.
+Promoção de alta coesão desmontando God classes em objetos menores e mais focados.
+O foco passou a ser em pedir para o objeto fazer algo com seus próprios dados, em vez de arrancar os dados dele para manipulá-los em outro lugar.
 
 YAGNI, KISS e SINE (YAGNI.py)
 YAGNI - Remoção de códigos especulativos e funcionalidades feitas para o caso de precisarmos no futuro. Foco no requisito atual.
